2807dc1eedfc2c01fc3000bf7eb9596852e5429a
mohammad ali masroor <masroor.1388@gmail.com>
2025-12-17 03:40:11 -0800

    logo

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -208,43 +208,38 @@ export default function MarketPage() {
  {/* لوگو برند - گوشه بالا سمت راست */}
 
 
- <div className="  flex items-center justify-center">
+  <div className="flex items-center justify-center gap-4">
+  <div className="w-15 h-15 bg-white rounded-full shadow-md flex items-center justify-center">
+    <a href="https://instagram.com/zarsimshop.ir" target="_blank" rel="noopener noreferrer">
+      <Image
+        src="/logo.jpg"
+        alt="ZarSim Logo"
+        width={55}
+        height={55}
+        className="object-contain ring-1 ring-gray-300 shadow-md rounded-full bg-black"
+        priority
+      />
+    </a>
+  </div>
 
- 
-<div className="absolute  w-15 h-15 bg-white rounded-full shadow-md z-10 flex items-center justify-center">
-  <a href="https://instagram.com/zarsimshop.ir" target="_blank" rel="noopener noreferrer">
-  <Image
-    src="/logo.jpg"
-    alt="ZarSim Logo"
-    width={55}
-    height={55}
-  className="object-contain ring-1 ring-gray-300 shadow-md rounded-full bg-black"
-    priority
-  />
-</a>
+  <div>
+    <a
+      referrerPolicy="origin"
+      target="_blank"
+      href="https://trustseal.enamad.ir/?id=685793&Code=noFb6UALV6gqz64VLxR44mdBpn5qD19C"
+    >
+      <img
+        referrerPolicy="origin"
+        src="https://trustseal.enamad.ir/logo.aspx?id=685793&Code=noFb6UALV6gqz64VLxR44mdBpn5qD19C"
+        alt=""
+        width={55}
+        height={55}
+        style={{ cursor: "pointer" }}
+      />
+    </a>
+  </div>
 </div>
 
-
- 
-
-
-
-    <a referrerPolicy='origin' 
-    target='_blank'
-     href='https://trustseal.enamad.ir/?id=685793&Code=noFb6UALV6gqz64VLxR44mdBpn5qD19C'>
-      <img
-       referrerPolicy='origin' 
-      src='https://trustseal.enamad.ir/logo.aspx?id=685793&Code=noFb6UALV6gqz64VLxR44mdBpn5qD19C' 
-      alt=''
-       width={55}
-        height={55}
-       style={{ cursor: "pointer" }}
-      // code='noFb6UALV6gqz64VLxR44mdBpn5qD19C'
-      />
-
-      </a>
-
-      </div>
 
     </div>
   </div>
